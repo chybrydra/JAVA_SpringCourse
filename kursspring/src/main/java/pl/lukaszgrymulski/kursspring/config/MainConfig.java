87be4ffe2bd25aba34c65be7a9f50c738737c62b
Łukasz Grymulski <lukasz.grymulski@gmail.com>
2019-03-20 15:22:10 +0100

    add basic spring security

--- kursspring/src/main/java/pl/lukaszgrymulski/kursspring/config/MainConfig.java
+++ kursspring/src/main/java/pl/lukaszgrymulski/kursspring/config/MainConfig.java
@@ -1,17 +1,19 @@
 package pl.lukaszgrymulski.kursspring.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import pl.lukaszgrymulski.kursspring.domain.repositories.DBKnightRepository;
 import pl.lukaszgrymulski.kursspring.domain.repositories.InMemoryRepository;
 import pl.lukaszgrymulski.kursspring.domain.repositories.KnightRepository;
 
 
 @Configuration
-public class MainConfig {
+public class MainConfig extends WebSecurityConfigurerAdapter {
 
     @Bean(name="InMemoryKnightRepository")
     @Profile("dev")
@@ -25,6 +27,14 @@ public class MainConfig {
     public KnightRepository createDBRepo(){
         KnightRepository repo = new DBKnightRepository();
         return repo;
+    }
+
+    @Autowired
+    public void securityUsers(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("{noop}admin").roles("ADMIN")
+                .and()
+                .withUser("anna").password("{noop}aaa").roles("USER");
     }
 
 }
